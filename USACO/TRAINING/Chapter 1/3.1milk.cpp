e9f70c97eeeca4405265ba020d7fd0609c9373b0
martinGeom <60369311+martinGeom@users.noreply.github.com>
2020-04-11 02:48:23 +0300

    Create 3.1milk.cpp

---
+++ USACO/TRAINING/Chapter 1/3.1milk.cpp
@@ -0,0 +1,34 @@
+/*
+ID: sah2020
+PROG: milk
+LANG: C++14
+*/
+//# include <iostream>
+# include <fstream>
+# include <algorithm>
+# include <vector>
+
+using namespace std;
+
+int main()
+{
+    ofstream cout ("milk.out");
+    ifstream cin ("milk.in");
+    int n, m;
+    vector<pair<int, int> > v;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
+    }
+    sort(v.begin(), v.end());
+    long long ans = 0;
+    for (int i = 0; n > 0; ++i)
+    {
+        ans += min(v[i].second, n) * v[i].first;
+        n -= min(v[i].second, n);
+    }
+    cout << ans << "\n";
+}
