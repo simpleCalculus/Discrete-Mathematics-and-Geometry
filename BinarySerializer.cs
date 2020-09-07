6288b3522be0080b2c9c572379b35b990099dc30
martinGeom <60369311+martinGeom@users.noreply.github.com>
2020-09-08 02:47:21 +0300

    Create BinarySerializer.cs

---
+++ BinarySerializer.cs
@@ -0,0 +1,26 @@
+public static class BinarySerializer
+{
+    public static byte[] Serialize<T>(T obj)
+    {
+        var serializer = new DataContractSerializer(typeof(T));
+        var stream = new MemoryStream();
+        using (var writer = 
+            XmlDictionaryWriter.CreateBinaryWriter(stream))
+        {
+            serializer.WriteObject(writer, obj);
+        }
+        return stream.ToArray();
+    }
+
+    public static T Deserialize<T>(byte[] data)
+    {
+        var serializer = new DataContractSerializer(typeof(T));
+        using (var stream = new MemoryStream(data))
+        using (var reader = 
+            XmlDictionaryReader.CreateBinaryReader(
+                stream, XmlDictionaryReaderQuotas.Max))
+        {
+            return (T)serializer.ReadObject(reader);
+        }
+    }
+}
