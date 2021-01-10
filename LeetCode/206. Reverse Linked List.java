d93609337bf7b831b9bd63a8b6c3e8f6b0ebb3ae
martinGeom <60369311+martinGeom@users.noreply.github.com>
2021-01-11 00:22:42 +0300

    Update 206. Reverse Linked List.java
    Reverse a singly linked list.
    https://leetcode.com/problems/reverse-linked-list/

--- LeetCode/206. Reverse Linked List.java
+++ LeetCode/206. Reverse Linked List.java
@@ -32,3 +32,22 @@ class Solution {
         return rev;
     }
 }
+
+/* Рекурсивный
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return null;
+        return helper(new ListNode(), head);
+    }
+    
+    public ListNode helper(ListNode node, ListNode head) {
+        node.val = head.val;
+        if (head.next == null)
+            return node;
+        ListNode x = new ListNode();
+        x.next = node;
+        return helper(x, head.next);
+    }
+}
+*/
